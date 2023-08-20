372f83c9d8384cf8d0d98b31277ab8a314c59a10
webngoc04 <126089189+webngoc04@users.noreply.github.com>
2023-08-20 15:46:34 +0700

    Create f.cpp

---
+++ f.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+long long ts(long long m)
+{
+	long long k = sqrt(m);
+	if(k*k==m)
+	{
+		return 1;
+	}
+	else{
+		return 0;
+	}
+}
+
+int main() {
+    long long n;
+    cin >> n;
+    long long p=n;
+    while(p--)
+    if(ts(p)==1)
+    {
+    	cout<<p;
+    	return 0;
+	}
+}
